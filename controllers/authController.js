5addd48213795c5591ca64a47732a1d089df64d4
ashugit1520 <ashutoshkumarr1520@gmail.com>
2025-09-21 01:40:10 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,44 +6,46 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 const authLogin = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(401).json({ error: 'Authentication failed' });
-        }
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
 
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) {
-            return res.status(401).json({ error: 'Authentication failed' });
-        }
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-            expiresIn: '1h',
-        });
-
-        res.cookie('access_token', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            maxAge: 3600000,
-            sameSite: 'none',
-            path: '/'
-        })
-            .status(200)
-            .json({ message: 'Logged in successfully' });
-
-    } catch (error) {
-        res.status(500).json({ error: 'Login failed' });
+    if (!user) {
+      return res.status(401).json({ error: "Authentication failed" });
     }
-}
 
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Authentication failed" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "none", 
+        maxAge: 24 * 60 * 60 * 1000, 
+        path: "/",
+      })
+      .status(200)
+      .json({ message: "Logged in successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Login failed" });
+  }
+};
 
 const authLogOut = (req, res) => {
-    res.clearCookie('access_token', {
-        httpOnly: true,
-        secure: true,
-      sameSite: 'none',
-        path: '/',
-    });
-    res.status(200).json({ message: 'Logged out successfully' });
-}
+  res.clearCookie("access_token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+    sameSite: "none",
+    path: "/",
+  });
+  res.status(200).json({ message: "Logged out successfully" });
+};
+
 export { authLogin, authLogOut };
